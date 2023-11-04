4f05ef02c59a58f1c1d4b29afccaee6dca29bad0
Henry Wrightson <henry.wrightson@digital.cabinet-office.gov.uk>
2023-11-04 22:33:19 +0000

    Create pipeline in ipynb to run process from start to finish. Note: untested, needs to be run when database schema is sorted

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-WEAVIATE_KEY = os.getenv("WEAVIATE_KEY")  
+WEAVIATE_KEY = os.getenv("WEAVIATE_KEY")
 auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_KEY)
-client = weaviate.Client(url="https://diagnosis-ai-generator-kk6m9y1x.weaviate.network", auth_client_secret=auth_config)  
+client = weaviate.Client(url="https://diagnosis-ai-generator-kk6m9y1x.weaviate.network", auth_client_secret=auth_config)
 
 client.schema.get()
